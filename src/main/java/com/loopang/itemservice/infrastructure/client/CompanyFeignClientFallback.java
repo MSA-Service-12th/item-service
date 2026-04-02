fc339e4ae398875d1619f69d15b687a01fcf2c7e
oni128 <lyw283828@gmail.com>
2026-04-02 21:11:58 +0900

    refactor: 폴백 문구 수정

--- src/main/java/com/loopang/itemservice/infrastructure/client/CompanyFeignClientFallback.java
+++ src/main/java/com/loopang/itemservice/infrastructure/client/CompanyFeignClientFallback.java
@@ -14,7 +14,7 @@ public class CompanyFeignClientFallback implements FallbackFactory<CompanyFeignC
     return id -> {
       log.error("[Company service Fallback] ID: {} 조회 중 장애 발생, 사유: {}",
           id, cause.getMessage(), cause); // 발생위치 -> 파생위치를 알려줌 stackTrace
-      throw new InternalServerErrorException("Hub Service API 요청 처리 실패, 잠시 후 다시 시도해주세요.");
+      throw new InternalServerErrorException("Company Service API 요청 처리 실패, 잠시 후 다시 시도해주세요.");
     };
   }
 }
