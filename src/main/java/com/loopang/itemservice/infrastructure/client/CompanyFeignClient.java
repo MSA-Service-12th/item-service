a3893760e76f4013275ffe806b8512439380e13d
oni128 <lyw283828@gmail.com>
2026-04-02 21:13:29 +0900

    refactor: 주석 제거 및 수정

--- src/main/java/com/loopang/itemservice/infrastructure/client/CompanyFeignClient.java
+++ src/main/java/com/loopang/itemservice/infrastructure/client/CompanyFeignClient.java
@@ -6,12 +6,12 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-// 대안을 자세한 로직
+
 @FeignClient(name = "company-service", fallbackFactory = CompanyFeignClientFallback.class)
 public interface CompanyFeignClient {
 
+    // json 형식으로 에러를 출력하기위해 글로벌로 안넘아가게 처리 -> try catch x
   @GetMapping("/companies/{id}")
   CompanyData get(@PathVariable UUID id);
-  // json 형식으로 에러를 출력하기위해 글로벌로 안넘아가게 처리.
 
 }
